ffb3a58ca0b29452448a92e1fba40ae9d706c138
YaSanyaBeats <alexandr191261@yandex.ru>
2025-11-08 18:13:54 +0700

    Redisign App Bar

--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -2,13 +2,9 @@ import React from 'react'
 import { getServerSession } from 'next-auth'
 import { redirect } from 'next/navigation'
 import { authOptions } from '../../lib/auth'
-import { Box, Stack, Paper } from '@mui/material'
-import Image from 'next/image'
-import Link from 'next/link'
-import LeftMenu from '@/components/leftMenu/LeftMenu'
-import HeaderMenu from '@/components/headerMenu/HeaderMenu'
 import ObjectsProvider from '@/providers/ObjectsProvider'
 import { getObjects } from '@/lib/beds24/objects'
+import MiniDrawer from '@/components/leftMenu/MiniDrawer'
 
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
@@ -21,28 +17,9 @@ export default async function RootLayout({ children }: { children: React.ReactNo
 
     return (
         <ObjectsProvider serverObjects={objects}>
-            <Box padding={3} height={'100vh'}>
-                <Stack spacing={2} height={'100%'}>
-                    <Paper elevation={2}>
-                        <Stack padding={2} direction="row" justifyContent={'space-between'}>
-                            <Link href="/dashboard">
-                                <Stack direction="row">
-                                    <Image src="/logo.svg" alt="HolyCow logo" width={90} height={40}></Image>
-                                </Stack>
-                            </Link>
-                            <HeaderMenu></HeaderMenu>
-                        </Stack>
-                    </Paper>
-                    <Stack spacing={2} direction="row" sx={{ flexGrow: '1' }}>
-                        <Paper elevation={2} sx={{ width: '300px' }}>
-                            <LeftMenu></LeftMenu>
-                        </Paper>
-                        <Paper elevation={2} sx={{ flexGrow: '1', padding: 2 }}>
-                            {children}
-                        </Paper>
-                    </Stack>
-                </Stack>
-            </Box>
+            <MiniDrawer>
+                {children}
+            </MiniDrawer>
         </ObjectsProvider>
     )
 }
---
+++ frontend/src/components/leftMenu/MiniDrawer.tsx
@@ -0,0 +1,220 @@
+'use client';
+
+import * as React from 'react';
+import { styled, Theme, CSSObject } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MuiDrawer from '@mui/material/Drawer';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Link from 'next/link'
+import { Dashboard, Analytics, PeopleAlt, MonetizationOn, Settings, House } from '@mui/icons-material';
+import styles from './leftMenu.module.css'
+import Image from 'next/image'
+
+const drawerWidth = 240;
+
+
+const openedMixin = (theme: Theme): CSSObject => ({
+width: drawerWidth,
+transition: theme.transitions.create('width', {
+easing: theme.transitions.easing.sharp,
+duration: theme.transitions.duration.enteringScreen,
+}),
+overflowX: 'hidden',
+});
+
+const closedMixin = (theme: Theme): CSSObject => ({
+    transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+}),
+overflowX: 'hidden',
+width: `calc(${theme.spacing(7)} + 1px)`,
+[theme.breakpoints.up('sm')]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+},
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+}));
+
+interface AppBarProps extends MuiAppBarProps {
+    open?: boolean;
+}
+
+const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+    })<AppBarProps>(({ theme }) => ({
+        zIndex: theme.zIndex.drawer + 1,
+        transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    variants: [
+        {
+            props: ({ open }) => open,
+            style: {
+            marginLeft: drawerWidth,
+            width: `calc(100% - ${drawerWidth}px)`,
+            transition: theme.transitions.create(['width', 'margin'], {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+            },
+        },
+    ],
+}));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+    ({ theme }) => ({
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+    boxSizing: 'border-box',
+    variants: [
+        {
+        props: ({ open }) => open,
+        style: {
+            ...openedMixin(theme),
+            '& .MuiDrawer-paper': openedMixin(theme),
+        },
+        },
+        {
+        props: ({ open }) => !open,
+        style: {
+            ...closedMixin(theme),
+            '& .MuiDrawer-paper': closedMixin(theme),
+        },
+        },
+    ],
+}),
+);
+
+const menu = [
+    { text: 'Главная', icon: <Dashboard fontSize="small" />, link: '/dashboard' },
+    { text: 'Аналитика', icon: <Analytics fontSize="small" />, link: '/dashboard/analytics' },
+    { text: 'Пользователи', icon: <PeopleAlt fontSize="small" />, link: '/dashboard/users' },
+    { text: 'Бухгалтерия', icon: <MonetizationOn fontSize="small" />, link: '/dashboard/accountancy' },
+    { text: 'Параметры', icon: <Settings fontSize="small" />, link: '/dashboard/options' },
+    { text: 'Beds24', icon: <House fontSize="small" />, link: '/dashboard/beds24' },
+];
+
+export default function MiniDrawer({ children }: { children: React.ReactNode }) {
+    const [open, setOpen] = React.useState(false);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <AppBar position="fixed" open={open}>
+                <Toolbar>
+                    <IconButton
+                    color="inherit"
+                    aria-label="open drawer"
+                    onClick={handleDrawerOpen}
+                    edge="start"
+                    sx={[
+                        {
+                        marginRight: 4,
+                        },
+                        open && { display: 'none' },
+                    ]}
+                    >
+                    <MenuIcon />
+                    </IconButton>
+                    <Link href="/dashboard">
+                        <Image src="/logo-new.svg" alt="HolyCow logo" width={90} height={40}></Image>
+                    </Link>
+                </Toolbar>
+            </AppBar>
+
+            <Drawer variant="permanent" open={open}>
+                <DrawerHeader>
+                    <IconButton onClick={handleDrawerClose}>
+                        <ChevronLeftIcon />
+                    </IconButton>
+                </DrawerHeader>
+                <Divider />
+
+                <List>
+                    {menu.map((item, index) => (
+                        <ListItem key={index} disablePadding sx={{ display: 'block' }}>
+                            <Link href={item.link} className={styles.link}>
+                                <ListItemButton
+                                sx={[
+                                    {
+                                    minHeight: 48,
+                                    px: 2.5,
+                                    },
+                                    open
+                                    ? {
+                                        justifyContent: 'initial',
+                                        }
+                                    : {
+                                        justifyContent: 'center',
+                                        },
+                                ]}
+                                >
+                                <ListItemIcon
+                                    sx={[
+                                    {
+                                        minWidth: 0,
+                                        justifyContent: 'center',
+                                    },
+                                    open
+                                        ? {
+                                            mr: 3,
+                                        }
+                                        : {
+                                            mr: 'auto',
+                                        },
+                                    ]}
+                                >
+                                    {item.icon}
+                                </ListItemIcon>
+                                <ListItemText
+                                    primary={item.text}
+                                    sx={[
+                                    open
+                                        ? {
+                                            opacity: 1,
+                                        }
+                                        : {
+                                            opacity: 0,
+                                        },
+                                    ]}
+                                />
+                                </ListItemButton>
+                            </Link>
+                        </ListItem>
+                    ))}
+                </List>
+            </Drawer>
+
+            <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+                <DrawerHeader />
+                {children}
+            </Box>
+        </Box>
+    );
+}
